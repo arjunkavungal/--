b436ff19736b4cdb44153337b5c47378802f7feb
yimen3 <47582621+yimen3@users.noreply.github.com>
2019-02-14 12:38:46 -0500

    Update noanu.py

--- noanu.py
+++ noanu.py
@@ -5,4 +5,8 @@ p = np.array([1,2,3])
 q = np.array([4,5,6])
 plt.plot(p,q)
 plt.show()
-print("You should be coded out, Anu");
+print("You should be coded out, Anu")
+print("Grayson's dad likes to drink")
+f = file.open("complaint.txt")
+for line in f:
+  print(line)

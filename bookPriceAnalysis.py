ee400390d6b0bff922af73022d5bd09b49a30bd3
yimen3 <47582621+yimen3@users.noreply.github.com>
2021-12-10 22:30:14 -0500

    Update bookPriceAnalysis.py

--- bookPriceAnalysis.py
+++ bookPriceAnalysis.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 import streamlit as st
 import pandas as pd
+import urllib.request, json 
 def foo_bar(x):
     return x['buyLink']
 form = st.form(key='my_form')
@@ -10,7 +11,6 @@ form = st.form(key='my-form')
 text_input = form.text_input('Enter your name')
 submit = form.form_submit_button('Submit')
 if submit:
-    import urllib.request, json 
     with urllib.request.urlopen("https://www.googleapis.com/books/v1/volumes?q="+str(text_input.replace(" ","+"))) as url:
         data = json.loads(url.read().decode())
         df = pd.DataFrame.from_dict(pd.DataFrame.from_dict(data['items']))

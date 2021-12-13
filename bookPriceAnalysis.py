959e987dc18bac44bd471ab7a143c4b92f01f03a
yimen3 <47582621+yimen3@users.noreply.github.com>
2021-12-13 00:10:43 -0500

    Update bookPriceAnalysis.py

--- bookPriceAnalysis.py
+++ bookPriceAnalysis.py
@@ -4,7 +4,7 @@ Spyder Editor
 
 This is a temporary script file.
 """
-from sklearn.ensemble import RandomForestClassifier
+
 import pandas as pd
 from urllib.request import urlopen
 import requests

13dbd8219a2b52f28e8604fe61f43d758e6b428b
yimen3 <47582621+yimen3@users.noreply.github.com>
2021-12-12 22:47:11 -0500

    Update bookPriceAnalysis.py

--- bookPriceAnalysis.py
+++ bookPriceAnalysis.py
@@ -22,7 +22,7 @@ if submit:
     response = requests.get("https://www.googleapis.com/books/v1/volumes?q=" + text_input.replace(" ","+")+ "&key=AIzaSyCYKIKheo-kxVkwr8Aq3468SbhIfXm_-C4&country=US")
     response_json = response.json()
     #df = pd.DataFrame.from_dict(reponse_json)
-    st.write(response_json)
+    st.write(response_json['items'])
 '''
 if submit:
     import urllib.request, json 

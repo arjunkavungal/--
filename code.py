cd5dda63b5ede17b00378be398cd15168f08f52b
yimen3 <47582621+yimen3@users.noreply.github.com>
2021-12-13 00:24:22 -0500

    Update code.py

--- code.py
+++ code.py
@@ -1,1 +1,4 @@
-
+response = requests.get("https://www.googleapis.com/books/v1/volumes?q=" + text_input.replace(" ","+")+ "&key=AIzaSyCYKIKheo-kxVkwr8Aq3468SbhIfXm_-C4&country=US")
+response_json = response.json()
+items = response_json['items']
+prices(items)

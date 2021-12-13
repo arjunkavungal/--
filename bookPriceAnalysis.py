e22592ff3a2a838e0c5f3be5c36836ed1865056e
yimen3 <47582621+yimen3@users.noreply.github.com>
2021-12-12 23:50:10 -0500

    Update bookPriceAnalysis.py

--- bookPriceAnalysis.py
+++ bookPriceAnalysis.py
@@ -21,7 +21,7 @@ def prices(dictionary):
             listPrices.append(listPrice['amount'])
             retailPrices.append(retailPrice['amount'])
     df = pd.DataFrame({"list price":listPrices,"retail price":retailPrices})
-    st.write(df)
+    st.line_chart(df)
 form = st.form(key='my_form')
 text_input = label='Enter some text'
 submit_button = form.form_submit_button(label='Submit')
